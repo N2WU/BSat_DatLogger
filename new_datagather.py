ad412731380feb1cb68bb1c5b8987ae4af82d836
N2WU <49031851+N2WU@users.noreply.github.com>
2021-06-09 10:04:00 -0600

    Update new_datagather.py
    Add CSV writing support

--- new_datagather.py
+++ new_datagather.py
@@ -1,4 +1,3 @@
-
 from gps import *
 import time
 import picamera
@@ -8,7 +7,11 @@ from time import sleep #fix this
 import smbus
 import csv
 
-writeCSV(gpsData, weatherData, index)
+#Row = [index, gpsData, weatherData]
+CSVHeaders = ["Index", "Latitude", "Longtitude", "Pressure (kPa)", "Altitude (m)", "Temperature (C)"] 
+with open('data.csv', 'w') as f: 
+    write = csv.writer(f) 
+    write.writerow(CSVHeaders) 
 
 def getPositionData(gps):
     nx = gpsd.next()
@@ -29,69 +32,43 @@ def getWeatherData():
     # MPL3115A2
     # This code is designed to work with the MPL3115A2_I2CS I2C Mini Module
     # Get I2C bus
-
     bus = smbus.SMBus(1)
-
     # MPL3115A2 address, 0x60(96)
     # Select control register, 0x26(38)
     # 0xB9(185) Active mode, OSR = 128, Altimeter mode
-
     bus.write_byte_data(0x60, 0x26, 0xB9)
-
     # MPL3115A2 address, 0x60(96)
     # Select data configuration register, 0x13(19)
     # 0x07(07) Data ready event enabled for altitude, pressure, temperature
-
     bus.write_byte_data(0x60, 0x13, 0x07)
-
     # MPL3115A2 address, 0x60(96)
     # Select control register, 0x26(38)
     # 0xB9(185) Active mode, OSR = 128, Altimeter mode
-
     bus.write_byte_data(0x60, 0x26, 0xB9)
-
     time.sleep(1)
-
     # MPL3115A2 address, 0x60(96)
     # Read data back from 0x00(00), 6 bytes
     # status, tHeight MSB1, tHeight MSB, tHeight LSB, temp MSB, temp LSB
-
     data = bus.read_i2c_block_data(0x60, 0x00, 6)
-
     # Convert the data to 20-bits
-
     tHeight = ((data[1] * 65536) + (data[2] * 256) + (data[3] & 0xF0)) / 16
-
     temp = ((data[4] * 256) + (data[5] & 0xF0)) / 16
-
     altitude = tHeight / 16.0
-
     cTemp = temp / 16.0
-
     fTemp = cTemp * 1.8 + 32
-
     # MPL3115A2 address, 0x60(96)
     # Select control register, 0x26(38)
     # 0x39(57) Active mode, OSR = 128, Barometer mode
-        
     bus.write_byte_data(0x60, 0x26, 0x39)
-
     time.sleep(1)
-
     # MPL3115A2 address, 0x60(96)
     # Read data back from 0x00(00), 4 bytes
     # status, pres MSB1, pres MSB, pres LSB
-
     data = bus.read_i2c_block_data(0x60, 0x00, 4)
-
     # Convert the data to 20-bits
-
     pres = ((data[1] * 65536) + (data[2] * 256) + (data[3] & 0xF0)) / 16
-
     pressure = (pres / 4.0) / 1000.0
-
     # Output data to screen
-
     # print "Pressure : %.2f kPa" %pressure
     # print "Altitude : %.2f m" %altitude
     # print "Temperature in Celsius : %.2f C" %cTemp
@@ -101,10 +78,10 @@ def getWeatherData():
 
          
 def writeCSV(gpsData, weatherData, index)
-    with open('data.csv','a') as fd:
-    
     Row = [index, gpsData, weatherData]
-    fd.write(Row)
+    with open(r'data.csv', 'a') as f:
+    writer = csv.writer(f)
+    writer.writerow(Row)
 
 # Next read from the pressure sensor    
 # Transmit telemetry every time the pictures are timestamped
@@ -138,7 +115,7 @@ while picCount < picTotal:
     timestampCommand = "/usr/bin/convert " + completeFilePath + " -pointsize 36 \
     -fill red -annotate +700+650 '" + timestampMessage + "' " + completeFilePath
     # Actually execute the command!
-    call(&#91;timestampCommand&#93;, shell=True)
+    call([timestampCommand], shell=True)
     print("We have timestamped our picture!")
     writeCSV(gpsData, weatherData, index)
     print("Written to CSV")
